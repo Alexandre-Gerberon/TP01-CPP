8a109795410a1439bf09a9ab1b153d9bad223c09
Alexandre-Gerberon <56343294+Alexandre-Gerberon@users.noreply.github.com>
2022-10-11 08:50:00 +0200

    Add header file

---
+++ partie1/partie1.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+int somme(int a, int b);
+void inversePointeur(int* a, int* b);
+void inverseReference(int& a, int& b);
+void remplacePointeur(int* a, int* b, int* c);
+void remplaceReference(int& a, int& b, int& c);
+void trie(std::vector<int>& tableau);
